bd8dac5b380d4ac41b8af1a6df916e970b327bb8
shalom250 <imanishimweshalom@gmail.com>
2026-08-13 16:23:54 +0000

    nhvmb

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import PublicLayout from "../../layouts/PublicLayout";
+
 import Home from "../../pages/Home/Home";
+import Register from "../../pages/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +13,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "register",
+        element: <Register />,
       },
     ],
   },
